a0578f5ce1dd863a601a675d419dd35a128e3c82
norus04 <noah.rushing@maine.edu>
2025-07-07 08:16:36 -0400

    Removed navbar links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,13 +6,6 @@ const Navbar = () => (
       <span className="logo-title">China Community Days</span>
       <span className="logo-sub">est. 2004</span>
     </div>
-    <ul className="navbar-links">
-      <li>Events</li>
-      <li>Businesses</li>
-      <li>Sign&#8209;Up</li>
-      <li>Pictures</li>
-      <li>Contact</li>
-    </ul>
   </nav>
 );
 
