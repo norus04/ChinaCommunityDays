d9900d9fd2998cc3f57a1571d18806bafdf2d1d8
norus04 <noah.rushing@maine.edu>
2025-07-11 10:11:26 -0400

    Updated vendors + descriptions

--- src/pages/VendorsPage.jsx
+++ src/pages/VendorsPage.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 
 const vendors = [
-  { name: 'China Four Seasons Club', description: 'Hot dogs' },
-  { name: 'Masons', description: 'Burgers' },
-  { name: "Piggy Sue's Food Truck", description: 'Fried ice cream + bacon steak skewers' },
-  { name: 'Raiders Sugarhouse', description: 'Maple syrup + cotton candy' },
-  { name: 'Party Palooga' },
-  { name: 'Tickles the Clown', description: 'Entertainment' },
-  { name: 'Sandcastle Entertainment', description: 'Water slide + bouncy house' },
-  { name: 'Make Em Your Way', description: 'Foam pit' },
-  { name: 'China Community Garden', description: 'Fresh produce' },
-  { name: 'R & F Design' },
-  { name: 'Bout Creating' },
-  { name: 'Make It Sweet Bakery' },
-  { name: 'South China American Legion' },
+  { name: 'China Four Seasons Club', description: 'Serving hot dogs and supporting local recreation' },
+  { name: 'Masons', description: 'Grilling up burgers' },
+  { name: "Piggy Sue's Food Truck", description: 'Fried ice cream and bacon steak skewers' },
+  { name: 'Raiders Sugarhouse', description: 'Maple syrup and fresh cotton candy' },
+  { name: 'Party Palooga', description: 'Inflatables and party fun for all ages' },
+  { name: 'Tickles the Clown', description: 'Interactive kids’ entertainment and balloon art' },
+  { name: 'Sandcastle Entertainment', description: 'Water slide and bounce house attractions' },
+  { name: 'Make Em Your Way', description: 'Running the foam pit' },
+  { name: 'China Community Garden', description: 'Fresh, local produce grown by the community' },
+  { name: 'R & F Design', description: 'Handmade crafts and custom creations' },
+  { name: 'Bout Creating', description: 'Creative goods from a local artisan' },
+  { name: 'Make It Sweet Bakery', description: 'Homemade baked goods and sweet treats' },
+  { name: 'South China American Legion', description: 'Supporting veterans with food and info' },
+  { name: 'Maine Photo Works', description: 'Local photography and videography' },
+  { name: 'Sy and Lei Laser Engraving', description: 'Laser-cut and engraved gifts' },
 ];
 
 const VendorsPage = () => (
