17b94c4595c401316673f66de24fd8fb3303c8dd
norus04 <noah.rushing@maine.edu>
2025-07-25 10:59:45 -0400

    Updated sign up form

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -64,31 +64,15 @@ const SignUpPage = () => {
           required
         >
           <option value="" disabled>Select</option>
-          <option value="Music by Brian Plato">Music by Brian Plato</option>
           <option value="Battle of the Buns (Cheeseburger Challenge)">Battle of the Buns (Cheeseburger Challenge)</option>
-          <option value="Community BBQ">Community BBQ</option>
-          <option value="Music by Yellow Brick Road (Elton John Tribute Band)">Music by Yellow Brick Road (Elton John Tribute Band)</option>
-          <option value="PTO Color Run">PTO Color Run</option>
-          <option value="Car Cruize-In">Car Cruize-In</option>
-          <option value="Vendors & Booths">Vendors & Booths</option>
-          <option value="Field Day">Field Day</option>
           <option value="Blow-Up Costume Race">Blow-Up Costume Race</option>
-          <option value="Food Booths">Food Booths</option>
           <option value="Boat Regatta">Boat Regatta</option>
-          <option value="Pickleball Tourney">Pickleball Tourney</option>
-          <option value="Tickles the Clown">Tickles the Clown</option>
-          <option value="Party Palooga">Party Palooga</option>
-          <option value="Foam Pit">Foam Pit</option>
-          <option value="China Fire Truck">China Fire Truck</option>
-          <option value="Carnival">Carnival</option>
-          <option value="Lighted Boat Parade">Lighted Boat Parade</option>
-          <option value="Laser Show by Dynamicks FX">Laser Show by Dynamicks FX</option>
-          <option value="Music by The Laura Hudson Project">Music by The Laura Hudson Project</option>
-          <option value="Church Service">Church Service</option>
-          <option value="Fishing Derby">Fishing Derby</option>
-          <option value="Scavenger Hunt">Scavenger Hunt</option>
-          <option value="Forest Walk (2 hour walk)">Forest Walk (2 hour walk)</option>
+          <option value="Car Cruize-In">Car Cruize-In</option>
           <option value="Forest Walk (1 hour walk)">Forest Walk (1 hour walk)</option>
+          <option value="Forest Walk (2 hour walk)">Forest Walk (2 hour walk)</option>
+          <option value="Music by Yellow Brick Road (Elton John Tribute Band)">Music by Yellow Brick Road (Elton John Tribute Band)</option>
+          <option value="Pickleball Tourney">Pickleball Tourney</option>
+          <option value="PTO Color Run">PTO Color Run</option>
           <option value="Volunteer">Volunteer</option>
         </select>
         <button type="submit">Submit</button>
